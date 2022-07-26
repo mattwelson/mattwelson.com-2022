130f5127c7bf81dd62870a187e5b2f4b504e1212
matt.welson <matt.welson@mtf.co.nz>
2022-07-26 18:08:54 +1200

    init index page

--- web/gatsby-node.ts
+++ web/gatsby-node.ts
@@ -1,16 +1,16 @@
-import type { GatsbyNode } from "gatsby"
+import type { GatsbyNode } from "gatsby";
 
 // Add a parentPost field and resolve it for SanityPosts
 // Note: @link didn't seem to work in this case
 export const createSchemaCustomization: GatsbyNode["createSchemaCustomization"] =
   ({ actions, schema }) => {
-    const { createTypes } = actions
+    const { createTypes } = actions;
     const typeDefs = [
       `
-  type SanityPost implements Node {
-    parentPost: SanityPost
-  }
-  `,
+      type SanityPost implements Node {
+        parentPost: SanityPost
+      }
+      `,
       schema.buildObjectType({
         name: "SanityPost",
         fields: {
@@ -24,12 +24,31 @@ export const createSchemaCustomization: GatsbyNode["createSchemaCustomization"] 
                     childPosts: { elemMatch: { id: { eq: source.id } } },
                   },
                 },
-              })
+              });
             },
           },
         },
       }),
-    ]
+    ];
 
-    createTypes(typeDefs)
-  }
+    createTypes(typeDefs);
+  };
+
+export const createPages: GatsbyNode["createPages"] = ({
+  graphql,
+  actions: { createPage },
+}) => {
+  // TODO:
+  // Query for allSanityPosts
+  // Iterate over all that do not have a parent as a "root" page,
+  // then iterate over remaining with their full paths (recursion?)
+  const sanityPosts = graphql(`
+    {
+      allSanityPost {
+        nodes {
+          id
+        }
+      }
+    }
+  `);
+};
